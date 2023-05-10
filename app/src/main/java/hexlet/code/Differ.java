bffa1109d8f4d9f32c9b52ef1477e5808488cd6f
DARYA SHCHEGLOVA <DaryaSHCH@github.com>
2023-05-10 18:37:12 +0400

    update Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.io.File;
@@ -14,11 +15,16 @@ import java.util.concurrent.Callable;
         description = "Compares two configuration files and shows a difference.")
 public class Differ implements Callable<String> {
 
-    @Parameters(index = "0")
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
     private File left;
 
-    @Parameters(index = "1")
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
     private File right;
+
+    @Option(names = {"-f", "--format"},
+            paramLabel = "format",
+            description = "output format [default: stylish]")
+    private String outputFormat;
 
     public static String generate(File filePath1, File filePath2){
         String differences = "";
