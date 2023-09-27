b6f2c891c0f43e623aa9c0d984232b0aca75f40e
DARYA SHCHEGLOVA <DaryaSHCH@github.com>
2023-09-27 18:42:09 +0400

    update DifferTest

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -2,15 +2,24 @@ package hexlet.code;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Parser {
     public static Map<String,Object> parseJson(File file) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
 
         return objectMapper.readValue(file, new TypeReference<Map<String,Object>>(){});
+    }
+    public static Map<String, Object> parseYml(File file) throws IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+        return objectMapper.readValue(file, new TypeReference<TreeMap<String, Object>>() {
+        });
+
     }
 }
--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -3,10 +3,13 @@ package hexlet.code;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import hexlet.code.Differ;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 class DifferTest {
-
-//    @Test
-//    void should
-
 }
