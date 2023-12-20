726c55897fc973bd28f2926122f29bfcf62a1763
DARYA SHCHEGLOVA <DaryaSHCH@github.com>
2023-12-20 22:30:09 +0400

    fix Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -6,6 +6,22 @@ import java.util.*;
 
 
 public class Differ {
+
+    public static String generate(String file1Path, String file2Path) throws IOException {
+        return generate(new File(file1Path), new File(file2Path), null);
+    }
+
+    public static String generate(String file1Path, String file2Path, String outputFormat) throws IOException {
+        OutputFormat outputFormatEnum;
+        if (outputFormat == null) {
+            outputFormatEnum = OutputFormat.STYLISH;
+        } else {
+            outputFormatEnum = OutputFormat.valueOf(outputFormat);
+        }
+
+        return generate(new File(file1Path), new File(file2Path), outputFormatEnum);
+    }
+
     public static String generate(File file1, File file2, OutputFormat outputFormat) throws IOException {
         Map<String, Object> leftData = Parser.parse(file1);
         Map<String, Object> rightData = Parser.parse(file2);
